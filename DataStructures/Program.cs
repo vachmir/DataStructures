276d29fcee3a3576aa99ea6cedfa030dc25c8360
Vachagan Mirzoian <vchgnmirms@gmail.com>
2021-12-02 22:46:29 +0400

    Improved LinkedLists and Lists

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,26 +8,77 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            LinkedList<int> intList = new LinkedList<int>();
+            LinkedList<int> intLinkedList = new LinkedList<int>();
+            List<int> intList = new List<int>();
             Stack<int> intStack = new Stack<int>();
             Queue<int> intQueue = new Queue<int>();
             Dictionary<int, bool> keyValuePairs = new Dictionary<int, bool>();
 
+            Console.WriteLine("LinkedList");
+            intLinkedList.AddLast(25);
+            intLinkedList.AddLast(26);
+            intLinkedList.AddLast(27);
+            intLinkedList.AddLast(28);
+            intLinkedList.AddLast(29);
+            intLinkedList.AddLast(30);
+            Console.WriteLine(intLinkedList.Count);
+            intLinkedList.RemoveFirst();
+            foreach (var item in intLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            intLinkedList.AddFirst(99);
+            Console.WriteLine("Linked List with new 1st element");
+            foreach (var number in intLinkedList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Number 28 removed ");
+
+            foreach (var number in intLinkedList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("List");
+            intList.Add(1);
+            intList.Add(5);
+            intList.Add(0);
+            intList.Add(4);
+            foreach (var item in intList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
             intStack.Push(5);
             intStack.Push(6);
             intStack.Push(7);
             intStack.Push(8);
             intStack.Push(9);
             intStack.Push(10);
-                        
-            Console.WriteLine(intStack.Peek());
-            Console.WriteLine(intStack.Peek());
+            Console.WriteLine("Initial Stack");
+            foreach (var item in intStack)
+            {
+                Console.WriteLine(item);
+            }
+            intStack.Peek();
+            intStack.Peek();
             intStack.Pop();
-            Console.WriteLine(intStack.Peek());
+            intStack.Peek();
             intStack.Push(2);
-            Console.WriteLine(intStack.Peek());
+            intStack.Peek();
+            Console.WriteLine("Changed Stack");            
+            foreach (var item in intStack)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("Queue");
 
             intQueue.Enqueue(15);
             intQueue.Enqueue(16);
@@ -35,53 +86,40 @@ namespace DataStructures
             intQueue.Enqueue(18); 
             intQueue.Enqueue(19);
             intQueue.Enqueue(20);
-            Console.WriteLine(intQueue.Peek());
-            Console.WriteLine(intQueue.Peek());
+            Console.WriteLine($"1st number now is: {intQueue.Peek()}");
+
+            foreach (var item in intQueue)
+            {
+                Console.WriteLine(item);
+            }
+
             intQueue.Dequeue();
-            Console.WriteLine(intQueue.Peek());
             intQueue.Enqueue(13);
-            Console.WriteLine(intQueue.Peek());
-              
-            Console.WriteLine();
-            Console.WriteLine();
-            intList.AddLast(25);
-            intList.AddLast(26);
-            intList.AddLast(27);
-            intList.AddLast(28);
-            intList.AddLast(29);
-            intList.AddLast(30);
-            Console.WriteLine(intList.Count);
-            //intList.RemoveFirst();
-            if (intList.Contains(25))
+            Console.WriteLine("1st number Dequeued, number 13 Enqueued");
+            Console.WriteLine($"1st number now is: {intQueue.Peek()}");
+            foreach (var item in intQueue)
             {
-                Console.WriteLine("25 exists");
-            }
-            Console.WriteLine();
-            intList.AddFirst(99);
-            Console.WriteLine();
-            foreach (var num in intList)
-            {
-                Console.WriteLine(num);
-            }
-            Console.WriteLine();
-            intList.Remove(28);
-
-            foreach (var num in intList)
-            {
-                Console.WriteLine(num);
+                Console.WriteLine(item);
             }
 
-            Console.WriteLine( );
             Console.WriteLine();
-            keyValuePairs.Add(1, true);
+            Console.WriteLine("Dictionaries");
+            keyValuePairs.Add(1, false);
+            keyValuePairs.Add(2, true);
+            keyValuePairs.Add(3, true);
+            keyValuePairs.Add(4, true);
             keyValuePairs.Add(5, true);
             keyValuePairs.Add(0, false);
-
             foreach (var item in keyValuePairs)
             {
                 Console.WriteLine(item.Key);
             }
-
+            keyValuePairs.Remove(2);
+            Console.WriteLine("Key 2 Removed");
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine($"Key is {item.Key}, Value is {item.Value}");
+            }           
         }
     }
 }
